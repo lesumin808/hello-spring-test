8a407f504e617958d63a6a650b0704ca5b1320b8
SUMIN <leesumin808@gmail.com>
2023-08-25 11:56:49 +0900

    MVC패턴 등록

--- src/main/java/hello/hellospringtest/controller/HelloController.java
+++ src/main/java/hello/hellospringtest/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospringtest.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +11,11 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","hello!!");
         return "hello";//viewreslover가 화면을 찾아서 처리 => 스프링 부트 템플릿 엔진은 기본 viewName 매핑 : resource/template에서 찾는다
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
