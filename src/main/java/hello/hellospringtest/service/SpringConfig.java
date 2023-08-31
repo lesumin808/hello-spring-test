5241d8b977293f17f66c90b183644b35ca894230
SUMIN <leesumin808@gmail.com>
2023-08-31 16:43:27 +0900

    통합 테스트 구현

--- src/main/java/hello/hellospringtest/service/SpringConfig.java
+++ src/main/java/hello/hellospringtest/service/SpringConfig.java
@@ -1,12 +1,23 @@
 package hello.hellospringtest.service;
 
+import hello.hellospringtest.repository.JdbcMemberRepository;
 import hello.hellospringtest.repository.MemberRepository;
 import hello.hellospringtest.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration//bean을 직접 넣어줄때
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -14,6 +25,6 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- src/test/java/hello/hellospringtest/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospringtest/service/MemberServiceIntegrationTest.java
@@ -1,33 +1,26 @@
 package hello.hellospringtest.service;
 
 import hello.hellospringtest.domain.Member;
+import hello.hellospringtest.repository.MemberRepository;
 import hello.hellospringtest.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.*;
-
-class MemberServiceTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
 
     //테스트는 한글로 변경이 가능 -> 직관적이게
-
+    @Autowired
     MemberService memberService;
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
-
-    @BeforeEach//각 메서드 실행하기 전에 실행시키는 것 (DI)
-    public void beforeEach(){
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach//테스트가 끝날때마다 데이터를 지워주는 콜백 메서드를 만들어 줘야된다.
-    public void afterEach(){
-        memberRepository.clearStore();
-    }
-
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     void 회원가입() {//join
@@ -66,11 +59,5 @@ class MemberServiceTest {
         //then
         //에러가 터져야된다
     }
-    @Test
-    void findMembers() {
-    }
 
-    @Test
-    void findOne() {
-    }
 }
